10aeb62d1a1b1754d507ec1162e9c7b4c1be0314
4ROKU <yr9601@gmail.com>
2022-02-11 22:33:39 +0900

    EmptyPage fix

--- dev-viewer/src/components/EmptyPage.js
+++ dev-viewer/src/components/EmptyPage.js
@@ -1,13 +1,17 @@
 import { Image, Button, Icon } from 'semantic-ui-react';
-import err from '../img/404error.jpg'
+import err from '../img/404error.png'
 
 export default function Empty(props) {
 
   return(
     <>
     <div className="empty-main">
-      <Image src={err} style={{margin:'auto'}}/>
-      <Button className='empty-back-btn' inverted color='red' onClick={() => props.history.goBack(1)}>
+      <Image className='empty-main-img' src={err}/>
+      <Button className='empty-home-btn' color='black' onClick={() => props.history.push("/")}>
+        <Icon name='home'/>
+        홈으로
+      </Button>
+      <Button className='empty-back-btn' color='grey' onClick={() => props.history.goBack(1)}>
         <Icon name='angle double left'/>
         이전 페이지로
       </Button>
