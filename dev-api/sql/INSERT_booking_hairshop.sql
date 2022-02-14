04d66c85548c8b3dbe227c426a5e0b28f37f04de
4ROKU <yr9601@gmail.com>
2022-02-15 00:09:19 +0900

    add create_date to booking func

--- dev-api/sql/INSERT_booking_hairshop.sql
+++ dev-api/sql/INSERT_booking_hairshop.sql
@@ -1,6 +1,6 @@
 INSERT INTO public.m_booking
-(user_cd,shop_cd,booking_category,booking_time,booking_price,booking_detail)
+(user_cd,shop_cd,booking_category,booking_time,booking_price,booking_detail,create_time)
 VALUES
 (':user_cd:',':shop_cd:',':booking_category:',':booking_time:',':booking_price:',
-JSON_OBJECT('designer',':designer:','style',':style:','discount',':discount:')
+JSON_OBJECT('designer',':designer:','style',':style:','discount',':discount:'),now()
 );
