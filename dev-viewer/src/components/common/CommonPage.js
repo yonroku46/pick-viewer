3f4b4201929fb62c972e64e95022cfecb5712d9c
4ROKU <71884535+yonroku46@users.noreply.github.com>
2022-03-23 21:50:23 +0900

    CommonPage fix

--- dev-viewer/src/components/common/CommonPage.js
+++ dev-viewer/src/components/common/CommonPage.js
@@ -1,24 +1,47 @@
 import React, { useEffect, useState, useReducer } from "react";
-import { Label, Container, Divider, Grid, Segment, Image, Icon } from 'semantic-ui-react'
+import { Menu, Form, Input, Grid, Segment, Image, Icon } from 'semantic-ui-react'
 import StackGrid from 'react-stack-grid';
 import * as api from '../../rest/api'
 import axios from 'axios';
 
 export default function CommonPage(props) {
 
-    const [consent, setConsent] = useState(false);
+    const [consents, setConsents] = useState(false);
     const [sp, setSp] = useState(false);
-    const postCd = '1';
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
         setSp(window.innerWidth < 767);
     }, [window.innerWidth])
 
+    function onCheckEnter(e) {
+        if (e.key === 'Enter') {
+            searching();
+        }
+    }
+
+    function delSearch() {
+        setSearch('');
+    }
+
+    function searching() {
+        console.log(search);
+    }
+
     return(
         <>
         <div className='common-main'>
+            <Menu.Item className='common-search'>
+                <Form onKeyPress={onCheckEnter}>
+                    <Input iconPosition='left' autoFocus={true} placeholder='검색' value={search} onChange={(e) => setSearch(e.target.value)}>
+                        {0 < search.length && <Icon className='search-del' name='times circle' onClick={delSearch}/>}
+                        <input className='search-input'/>
+                        <Icon className='search-btn' name='search' onClick={searching}/>
+                    </Input>
+                </Form>
+            </Menu.Item>
             <div className='common-posts'>
-                <StackGrid columnWidth={sp ? 360 : 260} gutterWidth={10} gutterHeight={10}>
+                <StackGrid columnWidth={sp ? 380 : 260} gutterWidth={8} gutterHeight={8}>
                     <figure>
                         <img src='http://localhost:3000/images/menu/1/1.png'/>
                         <figcaption>image1</figcaption>
@@ -30,6 +53,46 @@ export default function CommonPage(props) {
                     <figure>
                         <img src='http://localhost:3000/images/shop/default.png'/>
                         <figcaption>image2</figcaption>
+                    </figure>
+                    <figure>
+                        <img src='http://localhost:3000/images/menu/default.png'/>
+                        <figcaption>image3</figcaption>
+                    </figure>
+                    <figure>
+                        <img src='http://localhost:3000/images/menu/1/1.png'/>
+                        <figcaption>image1</figcaption>
+                    </figure>
+                    <figure>
+                        <img src='http://localhost:3000/images/menu/1/2.png'/>
+                        <figcaption>image4</figcaption>
+                    </figure>
+                    <figure>
+                        <img src='http://localhost:3000/images/shop/default.png'/>
+                        <figcaption>image2</figcaption>
+                    </figure>
+                    <figure>
+                        <img src='http://localhost:3000/images/menu/default.png'/>
+                        <figcaption>image3</figcaption>
+                    </figure>
+                    <figure>
+                        <img src='http://localhost:3000/images/menu/default.png'/>
+                        <figcaption>image3</figcaption>
+                    </figure>
+                    <figure>
+                        <img src='http://localhost:3000/images/menu/1/1.png'/>
+                        <figcaption>image1</figcaption>
+                    </figure>
+                    <figure>
+                        <img src='http://localhost:3000/images/menu/1/2.png'/>
+                        <figcaption>image4</figcaption>
+                    </figure>
+                    <figure>
+                        <img src='http://localhost:3000/images/shop/default.png'/>
+                        <figcaption>image2</figcaption>
+                    </figure>
+                    <figure>
+                        <img src='http://localhost:3000/images/menu/default.png'/>
+                        <figcaption>image3</figcaption>
                     </figure>
                     <figure>
                         <img src='http://localhost:3000/images/menu/default.png'/>
