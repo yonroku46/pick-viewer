eaadf09f5e6be04c7b5b3d388317dc37a69a5866
4ROKU <71884535+yonroku46@users.noreply.github.com>
2022-03-28 00:08:40 +0900

    api setting fix

--- dev-api/src/setting.py
+++ dev-api/src/setting.py
@@ -23,8 +23,7 @@ class ProductionConfig(Config):
         maxconnections=6,  #  maximum number of connections allowed in connection pool, 0 and none indicate that the number of connections is not limited 
         mincached=2,  #  at least idle links created in the link pool when initialized ，0 means not created 
         maxcached=5,  #  the most idle links in the link pool, 0 and none 
-        maxshared=3,
-        #  the maximum number of links shared in the link pool, 0 and none represent all shared 。PS:  useless because pymysql and mysqldb module  threadsafety they're all 1, whatever the value is ，_maxcached always zero, so always all links are shared 。
+        maxshared=3,  #  the maximum number of links shared in the link pool, 0 and none represent all shared 。PS:  useless because pymysql and mysqldb module  threadsafety they're all 1, whatever the value is ，_maxcached always zero, so always all links are shared 。
         blocking=True,  #  whether to block and wait after no connection is available in the connection pool 。True， waiting for the ；False， don't wait and report an error 
         maxusage=None,  #  the maximum number of times a link can be reused, None express unlimited 
         setsession=[],  #  list of commands executed before starting the session. such as ：["set datestyle to ...", "set time zone ..."]
@@ -45,8 +44,7 @@ class DevelopmentConfig(Config):
         maxconnections=6,  #  maximum number of connections allowed in connection pool, 0 and none indicate that the number of connections is not limited 
         mincached=2,  #  at least idle links created in the link pool when initialized ，0 means not created 
         maxcached=5,  #  the most idle links in the link pool, 0 and none 
-        maxshared=3,
-        #  the maximum number of links shared in the link pool, 0 and none represent all shared 。PS:  useless because pymysql and mysqldb module  threadsafety they're all 1, whatever the value is ，_maxcached always zero, so always all links are shared 。
+        maxshared=3,  #  the maximum number of links shared in the link pool, 0 and none represent all shared 。PS:  useless because pymysql and mysqldb module  threadsafety they're all 1, whatever the value is ，_maxcached always zero, so always all links are shared 。
         blocking=True,  #  whether to block and wait after no connection is available in the connection pool 。True， waiting for the ；False， don't wait and report an error 
         maxusage=None,  #  the maximum number of times a link can be reused, None express unlimited 
         setsession=[],  #  list of commands executed before starting the session. such as ：["set datestyle to ...", "set time zone ..."]
