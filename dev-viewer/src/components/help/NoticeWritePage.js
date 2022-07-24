b050cca2b968ac9cf3ab95b521ee7bbf53eefa53
4ROKU <71884535+yonroku46@users.noreply.github.com>
2022-07-24 22:43:20 +0900

    NoticeWritePage UI fix

--- dev-viewer/src/components/help/NoticeWritePage.js
+++ dev-viewer/src/components/help/NoticeWritePage.js
@@ -3,7 +3,7 @@ import * as api from '../../rest/api'
 import axios from 'axios';
 import { Editor } from '@toast-ui/react-editor';
 import { Link, withRouter } from "react-router-dom";
-import { Table, Icon, Header, Button, Input } from 'semantic-ui-react';
+import { Icon, Dropdown, Header, Button, Input, Checkbox } from 'semantic-ui-react';
 import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import 'tui-color-picker/dist/tui-color-picker.css';
@@ -11,26 +11,47 @@ import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-sy
 
 function NoticeWritePage(props) {
 
+    const categoryOptions = [
+        { key: 'n', text: '공지사항', value: 'notice' },
+        { key: 'p', text: '패치노트', value: 'patch' },
+        { key: 'e', text: '이벤트', value: 'event' },
+    ]
+    const [category, setCategory] = useState(categoryOptions[0].value);
+    const [title, setTitle] = useState('');
+    const [activate, setActivate] = useState(true);
     const editorRef = useRef();
 
-    const handleRegisterButton = () => {
-        // 입력창에 입력한 내용을 HTML 태그 형태로 취득
-        console.log(editorRef.current?.getInstance().getHTML());
-        // 입력창에 입력한 내용을 MarkDown 형태로 취득
-        console.log(editorRef.current?.getInstance().getMarkdown());
-      };
+    function noticeSubmit() {
+        if (window.confirm(activate ? "본 내용을 저장 후 공개 하시겠습니까?" : "비공개 상태입니다. 본 내용을 저장하시겠습니까?")) {
+            const contentHTML = editorRef.current?.getInstance().getHTML();
+            const contentMarkDown = editorRef.current?.getInstance().getMarkdown();
+            console.log(category, title, activate)
+            console.log(contentHTML)
+        } else {
+            return;
+        }
+    };
 
     return(
     <>
     <div className='notice-main'>
         <div className='notice-editor'>
-            <Header as='h2' className='header'>게시글 작성</Header>
-            <Input className='title' size='small' placeholder='제목을 입력해주세요.'/>
+            <Header as='h4'><Icon name='write square'/>게시글 작성</Header>
+            <div className='notice-editor-title'>
+                <Dropdown 
+                    selection
+                    className='category'
+                    options={categoryOptions} 
+                    value={category} 
+                    placeholder={categoryOptions[0]}
+                    onChange={(e, { value }) => setCategory(value)}/>
+                <Input size='small' className='title' placeholder='제목을 입력해주세요.' value={title} onChange={(e, { value }) => setTitle(value)}/>
+            </div>
             <Editor
                 ref={editorRef}
                 placeholder="내용을 입력해주세요."
                 previewStyle="vertical"
-                height="580px"
+                height="540px"
                 initialEditType="wysiwyg" // 초기 입력모드 설정(디폴트 markdown)
                 plugins={[colorSyntax]}
                 toolbarItems={[
@@ -41,10 +62,12 @@ function NoticeWritePage(props) {
                     ['code', 'codeblock']
                 ]}
             />
-            <Button primary className='submit' onClick={handleRegisterButton}>저장</Button>
+            <div className='notice-editor-bottom'>
+                <Button secondary className='submit' onClick={noticeSubmit}>저장</Button>
+                <Checkbox label={activate ? <Icon size='large' name='eye'/> : <Icon size='large' name='eye slash'/>} checked={activate} onChange={(e, { checked }) => setActivate(checked)}/>
+            </div>
         </div>
     </div>
-    <script src="ckeditor/ckeditor.js"></script>
     </>
     )
   };
